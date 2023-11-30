033265b6d383313227cfa5588f14d5016040b7d2
Giuseppe Albamonte <giualbamonte06@gmail.com>
2023-11-30 11:51:44 +0100

    Update Libreria.java

--- libreria/src/Libreria/Libreria.java
+++ libreria/src/Libreria/Libreria.java
@@ -4,16 +4,19 @@ import java.util.ArrayList;
 
 public class Libreria {
     private int nScaffli=0;
-    private ArrayList<Scaffale> Scaffali = new ArrayList<Scaffale>();
+    private ArrayList<Scaffale> scaffali = new ArrayList<Scaffale>();
 
     public void spostaLibro(int idLibro, int newScaffale){
-
+    	int pScaffale = Integer.parseInt(Integer.toString(idLibro).substring(0, 0));
+    	Libro libro = scaffali.get(pScaffale-1).getLibro(idLibro);
+    	scaffali.get(pScaffale-1).eliminaLibro(idLibro);
+    	scaffali.get(newScaffale).aggiungiLibro(libro);
     }
 
-    public void aggiungiLibro(int nScaffale, int idLibro, String titolo, String autore, String editore, float larghezza_cm){
-
+    public void aggiungiLibro(int nScaffale, String titolo, String autore, String editore, float larghezza_cm){
+    	scaffali.get(nScaffale-1).aggiungiLibro(scaffali.size(), titolo, autore, editore, larghezza_cm);
     }
     public void aggiungiScaffale(float lunghezza_cm){
-
+    	scaffali.add(new Scaffale(scaffali.size(), lunghezza_cm));
     }
 }
