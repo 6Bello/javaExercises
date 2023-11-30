bf0190c8e267455a1007a595f8e9f35977c948d6
Giuseppe Albamonte <giualbamonte06@gmail.com>
2023-11-30 11:29:20 +0100

    Update Libro.java

--- libreria/src/Libreria/Libro.java
+++ libreria/src/Libreria/Libro.java
@@ -2,14 +2,31 @@ package Libreria;
 
 public class Libro {
     private int idLibro;
+    public int getIdLibro(){
+        return idLibro;
+    }
+    void setIdLibro(int idLibro){
+        this.idLibro = idLibro;
+    }
     private int nScaffale;
     private String titolo;
+
+    public String getTitolo(){
+        return titolo;
+    }
     private float larghezza_cm;
     private String autore;
     private String editore;
 
-    Libro(int nScaffale, String titolo, String autore, String editore, float larghezza_cm){
-
+    Libro(int idLibro, String titolo, String autore, String editore, float larghezza_cm){
+        this.idLibro = idLibro;
+        while (nScaffale>=10){
+            this.nScaffale = idLibro/10;
+        }
+        this.titolo = titolo;
+        this.autore = autore;
+        this.editore = editore;
+        this.larghezza_cm = larghezza_cm;
     }
 
 }
