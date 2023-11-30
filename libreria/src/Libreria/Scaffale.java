e412d5cf27d655272e4fce8d7d20b195ed3ef64e
Giuseppe Albamonte <giualbamonte06@gmail.com>
2023-11-30 11:28:37 +0100

    Update Scaffale.java

--- libreria/src/Libreria/Scaffale.java
+++ libreria/src/Libreria/Scaffale.java
@@ -1,25 +1,49 @@
 package Libreria;
 
+import java.util.ArrayList;
+
 public class Scaffale {
     private int nLibri = 0;
     private float lunghezza_cm = 0;
-    private Libro libri;
+    private ArrayList<Libro> libri = new ArrayList<Libro>();
 
-    Scaffale(flat lunghezza_cm){
-
+    Scaffale(float lunghezza_cm){
+        this.lunghezza_cm = lunghezza_cm;
     }
 
     public float getLunghezza(){
         return lunghezza_cm;
     }
-    public void aggiungiLibro(int nScaffale, String titolo, String autore, String editore, float larghezza_cm){
-
+    public void aggiungiLibro(int id, String titolo, String autore, String editore, float larghezza_cm){
+        libri.add(new Libro(id, titolo, autore, editore, larghezza_cm));
     }
 
     public void eliminaLibro(String nomeLibro){
-
+        for(int i = 0; i<libri.size(); i++){
+            if(libri.get(i).getTitolo()==nomeLibro) {
+                libri.remove(i);
+                return;
+            }
+        }
     }
     public void eliminaLibro(int idLibro){
-        
+        for(int i = 0; i<libri.size(); i++){
+            if(libri.get(i).getIdLibro()==idLibro) {
+                libri.remove(i);
+                return;
+            }
+        }
+    }
+
+    private void ordinamentoScaffale(){
+        int sizeLibri = libri.size();
+        for(int i = 0; i<sizeLibri; i++)  {
+            Libro libro = libri.get(i);
+            int idLibro = libro.getIdLibro()
+            int idLibroSenzaNScaffale = Integer.parseInt(Integer.toString(idLibro.substring(1)));
+            if(idLibroSenzaNScaffale>sizeLibri){
+                libro.setIdLibro(idLibro++);
+            }
+        }
     }
 }
